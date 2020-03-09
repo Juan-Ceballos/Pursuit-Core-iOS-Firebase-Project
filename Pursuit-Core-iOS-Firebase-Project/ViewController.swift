48fb76145682e6b11a4f32a5908a7c1749045216
Juan C <juanceballos@pursuit.org>
2020-03-09 15:49:07 -0400

    fc

---
+++ Pursuit-Core-iOS-Firebase-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pursuit-Core-iOS-Firebase-Project
+//
+//  Created by Juan Ceballos on 3/9/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pursuit-Core-iOS-Firebase-ProjectTests/Pursuit_Core_iOS_Firebase_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pursuit_Core_iOS_Firebase_ProjectTests.swift
+//  Pursuit-Core-iOS-Firebase-ProjectTests
+//
+//  Created by Juan Ceballos on 3/9/20.
+//  Copyright © 2020 Juan Ceballos. All rights reserved.
+//
+
+import XCTest
+@testable import Pursuit_Core_iOS_Firebase_Project
+
+class Pursuit_Core_iOS_Firebase_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
